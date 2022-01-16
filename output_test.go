b8052654853c86fedb740d1d4c6d0cdcc33ccde6
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-01-16 23:19:35 +0700

    Output

---
+++ output_test.go
@@ -0,0 +1,18 @@
+package belajar_golang_logging
+
+import (
+	"github.com/sirupsen/logrus"
+	"os"
+	"testing"
+)
+
+func TestOutput(t *testing.T) {
+	logger := logrus.New()
+
+	file, _ := os.OpenFile("application.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	logger.SetOutput(file)
+
+	logger.Info("Hello Logging")
+	logger.Warn("Hello Logging")
+	logger.Error("Hello Logging")
+}
